78a10eadaac4137ab6fdd0ea4b5b97e3408f2d6d
JoeBakalor <jmbakalor@gmail.com>
2024-04-16 07:40:13 -0500

    Initial Commit

---
+++ HPDemo/HPDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HPDemoApp.swift
+//  HPDemo
+//
+//  Created by Joe Bakalor on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct HPDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
